1faa35e8e521f8471e8d6096f3fc358bbae3f796
afrankl <afrankl@berkeley.edu>
2016-09-13 17:37:22 -0700

    added lines to gitignore

--- resume/static/app/js/angular/directive.js
+++ resume/static/app/js/angular/directive.js
@@ -37,38 +37,6 @@
     }
 })();
 (function (){
-    "use-strict";
-
-    angular.module('app.content')
-        .directive('resumeContent', resumeContentDirective);
-
-    resumeContentDirective.$inject = ['$rootScope'];
-
-    function resumeContentDirective($rootScope) {
-        return {
-            restrict: 'EA',
-            templateUrl: 'static/app/templates/base/content/content.directive.html',
-            controllerAs: 'vm',
-            controller: resumeContentController,
-            scope: {},
-            transclude: true,
-            bind: {},
-            link: function(scope, element, attrs, ctrl) {
-                    $rootScope.$watch('navOpen', function(newVal, oldVal){
-                        ctrl.navOpen = newVal;
-                    });
-                }
-        };
-    }
-
-    resumeContentController.$inject = ['$state', '$rootScope'];
-    
-    function resumeContentController($state, $rootScope) {
-        var vm = this;
-        vm.navOpen = !$rootScope.navOpen;
-    }
-})();
-(function (){
 
     "use-strict";
 
@@ -189,6 +157,38 @@
             vm.navOpen = !$rootScope.navOpen
             $rootScope.navOpen = vm.navOpen;
         }
+    }
+})();
+(function (){
+    "use-strict";
+
+    angular.module('app.content')
+        .directive('resumeContent', resumeContentDirective);
+
+    resumeContentDirective.$inject = ['$rootScope'];
+
+    function resumeContentDirective($rootScope) {
+        return {
+            restrict: 'EA',
+            templateUrl: 'static/app/templates/base/content/content.directive.html',
+            controllerAs: 'vm',
+            controller: resumeContentController,
+            scope: {},
+            transclude: true,
+            bind: {},
+            link: function(scope, element, attrs, ctrl) {
+                    $rootScope.$watch('navOpen', function(newVal, oldVal){
+                        ctrl.navOpen = newVal;
+                    });
+                }
+        };
+    }
+
+    resumeContentController.$inject = ['$state', '$rootScope'];
+    
+    function resumeContentController($state, $rootScope) {
+        var vm = this;
+        vm.navOpen = !$rootScope.navOpen;
     }
 })();
 (function () {
