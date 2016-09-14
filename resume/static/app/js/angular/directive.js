007d547b3e7398ba235038294e84b4781d4fc33c
afrankl <afrankl@berkeley.edu>
2016-09-14 15:24:10 -0700

    INIT

--- resume/static/app/js/angular/directive.js
+++ resume/static/app/js/angular/directive.js
@@ -37,6 +37,38 @@
     }
 })();
 (function (){
+    "use-strict";
+
+    angular.module('app.content')
+        .directive('resumeContent', resumeContentDirective);
+
+    resumeContentDirective.$inject = ['$rootScope'];
+
+    function resumeContentDirective($rootScope) {
+        return {
+            restrict: 'EA',
+            templateUrl: 'static/app/templates/base/content/content.directive.html',
+            controllerAs: 'vm',
+            controller: resumeContentController,
+            scope: {},
+            transclude: true,
+            bind: {},
+            link: function(scope, element, attrs, ctrl) {
+                    $rootScope.$watch('navOpen', function(newVal, oldVal){
+                        ctrl.navOpen = newVal;
+                    });
+                }
+        };
+    }
+
+    resumeContentController.$inject = ['$state', '$rootScope'];
+    
+    function resumeContentController($state, $rootScope) {
+        var vm = this;
+        vm.navOpen = !$rootScope.navOpen;
+    }
+})();
+(function (){
 
     "use-strict";
 
@@ -157,38 +189,6 @@
             vm.navOpen = !$rootScope.navOpen
             $rootScope.navOpen = vm.navOpen;
         }
-    }
-})();
-(function (){
-    "use-strict";
-
-    angular.module('app.content')
-        .directive('resumeContent', resumeContentDirective);
-
-    resumeContentDirective.$inject = ['$rootScope'];
-
-    function resumeContentDirective($rootScope) {
-        return {
-            restrict: 'EA',
-            templateUrl: 'static/app/templates/base/content/content.directive.html',
-            controllerAs: 'vm',
-            controller: resumeContentController,
-            scope: {},
-            transclude: true,
-            bind: {},
-            link: function(scope, element, attrs, ctrl) {
-                    $rootScope.$watch('navOpen', function(newVal, oldVal){
-                        ctrl.navOpen = newVal;
-                    });
-                }
-        };
-    }
-
-    resumeContentController.$inject = ['$state', '$rootScope'];
-    
-    function resumeContentController($state, $rootScope) {
-        var vm = this;
-        vm.navOpen = !$rootScope.navOpen;
     }
 })();
 (function () {
