49e053e4772a0297050a204828dd0b74c18f4594
afrankl <afrankl@berkeley.edu>
2016-09-21 15:45:09 -0700

    Added viewport and links topnav item

--- resume/static/app/js/angular/directive.js
+++ resume/static/app/js/angular/directive.js
@@ -33,6 +33,71 @@
         }
     }
 })();
+(function () {
+
+    "use-strict";
+
+    angular.module('app.topnav', [])
+        .directive('resumeTopNav', resumeTopNavDirective);
+
+    function resumeTopNavDirective(){
+        return {
+            restrict: 'EA',
+            templateUrl: '/static/app/templates/base/topnav/topnav.directive.html',
+            controllerAs: 'vm',
+            controller: resumeTopNavController,
+            scope: {},
+            bind: {},
+            transclude: true,
+        }
+    }
+
+    resumeTopNavController.$inject = ['$state', '$rootScope', '$compile', '$scope']
+
+    function resumeTopNavController($state, $rootScope, $compile, $scope) {
+        //vars
+        var vm = this;
+        vm.navOpen = $rootScope.navOpen;
+
+        //functions
+        vm.onMenuClicked = onMenuClicked;
+        vm.onDownloadResumeClicked = onDownloadResumeClicked;
+        vm.resumeElement = $compile('<resume></resume>')($scope);
+
+        function onMenuClicked() {
+            vm.navOpen = !$rootScope.navOpen
+            $rootScope.navOpen = vm.navOpen;
+        }
+
+        function onDownloadResumeClicked() {
+            resumeToPng(vm.resumeElement);
+        }
+
+        function resumeToPng(resumeElement) {
+            var resumeContent = resumeElement[0].childNodes[0];
+            var content = document.getElementById('content');
+            var exists = true;
+            if ($('#my-content').length == 0) {
+                content.appendChild(resumeContent);
+                exists = false;
+            } else {
+                resumeContent = document.getElementById('my-content');
+            }
+            var header = $('#my-content h1#adjusted-header');
+            header.css('margin-top', '-9px');
+            domtoimage.toBlob(resumeContent).then(function(blob) {
+                saveAs(blob, "Avi-Frankl-Resume.png");
+                header.css('margin-top', '0px');
+                if (!exists) {
+                    content.removeChild(resumeContent); 
+                }
+                vm.resumeElement = $compile('<resume></resume>')($scope);
+            }).catch(function (error) {
+                console.log(error);
+            })
+        }
+    }
+})();
 (function (){
     "use-strict";
 
@@ -151,71 +216,6 @@
             $state.go(sref);
         }
 
-    }
-})();
-(function () {
-
-    "use-strict";
-
-    angular.module('app.topnav', [])
-        .directive('resumeTopNav', resumeTopNavDirective);
-
-    function resumeTopNavDirective(){
-        return {
-            restrict: 'EA',
-            templateUrl: '/static/app/templates/base/topnav/topnav.directive.html',
-            controllerAs: 'vm',
-            controller: resumeTopNavController,
-            scope: {},
-            bind: {},
-            transclude: true,
-        }
-    }
-
-    resumeTopNavController.$inject = ['$state', '$rootScope', '$compile', '$scope']
-
-    function resumeTopNavController($state, $rootScope, $compile, $scope) {
-        //vars
-        var vm = this;
-        vm.navOpen = $rootScope.navOpen;
-
-        //functions
-        vm.onMenuClicked = onMenuClicked;
-        vm.onDownloadResumeClicked = onDownloadResumeClicked;
-        vm.resumeElement = $compile('<resume></resume>')($scope);
-
-        function onMenuClicked() {
-            vm.navOpen = !$rootScope.navOpen
-            $rootScope.navOpen = vm.navOpen;
-        }
-
-        function onDownloadResumeClicked() {
-            resumeToPng(vm.resumeElement);
-        }
-
-        function resumeToPng(resumeElement) {
-            var resumeContent = resumeElement[0].childNodes[0];
-            var content = document.getElementById('content');
-            var exists = true;
-            if ($('#my-content').length == 0) {
-                content.appendChild(resumeContent);
-                exists = false;
-            } else {
-                resumeContent = document.getElementById('my-content');
-            }
-            var header = $('#my-content h1#adjusted-header');
-            header.css('margin-top', '-9px');
-            domtoimage.toBlob(resumeContent).then(function(blob) {
-                saveAs(blob, "Avi-Frankl-Resume.png");
-                header.css('margin-top', '0px');
-                if (!exists) {
-                    content.removeChild(resumeContent); 
-                }
-                vm.resumeElement = $compile('<resume></resume>')($scope);
-            }).catch(function (error) {
-                console.log(error);
-            })
-        }
     }
 })();
 (function () {
